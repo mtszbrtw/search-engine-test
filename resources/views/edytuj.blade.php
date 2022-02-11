1a60099689166e5ab396207e24f41ca13888f3c8
mtszbrtw <mtszbrtw@gmail.com>
2022-02-11 16:16:23 +0100

    commit two

---
+++ resources/views/edytuj.blade.php
@@ -0,0 +1,11 @@
+<form action="{{ route('edytuj.edytuj',['id'=>$record->id]) }}" method="put">
+    edytujesz rekord id {{ $record->id }}<br>
+    @csrf
+
+    <label for="title"></label><input value="{{ $record->title }}" type="text" id="title" name="title" placeholder="Podaj tytuł" required><br>
+    <label for="description"></label><input id="description" name="description" placeholder="podaj opis" required type="text"
+           value="{{ $record->description }}"><br>
+    <label for="created_at"></label><input value="{{ $record->created_at }}" type="text" id="created_at" name="created_at" required><br>
+
+    <input type="submit" value="edytuj">
+</form>
---
+++ resources/views/userform.blade.php
@@ -0,0 +1,9 @@
+<form method="post">
+
+
+    <label for="title"></label><input id="title" name="title" placeholder="Podaj tytuł" required type="text"><br>
+    <label for="description"></label><input type="text" id="description" name="description" placeholder="podaj opis" required><br>
+    <label for="created_at"></label><input id="created_at" name="created_at" required type="date"><br>
+
+    <input type="submit" value="Zapisz">
+</form>
